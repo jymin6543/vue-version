1ba3a120819ca765ca78fb62110cef84939dc0c0
jymin6543 <jy.min6543@gmail.com>
2018-06-14 16:46:48 +0900

    First component complete

---
+++ src/pages/Home.vue
@@ -0,0 +1,132 @@
+<template>
+<div id="SyeP-ORITf">
+  <div class="BJ3f_RUTG">
+    <table class="BkTOhRIaM">
+      <tr class="rkeaOhCL6M">
+        <th class="S1bpO3CL6M">head 1</th>
+      
+        <th class="Hkzp_2RLaM">head 2</th>
+      
+        <th class="BJXpunC86M">head 3</th>
+      </tr>
+    
+      <tr class="H14pOhCLTG">
+        <td class="rJBpd3CUpf">data 1.1</td>
+      
+        <td class="rk86d30Lpf">data 2.1</td>
+      
+        <td class="rJvTOhAUpz">data 3.1</td>
+      </tr>
+    
+      <tr class="r1_6O2CLTz">
+        <td class="Bktad3CLpG">data 1.2</td>
+      
+        <td class="Sk9pO2CIpf">data 2.2</td>
+      
+        <td class="r1ip_2AI6G">data 3.2</td>
+      </tr>
+    
+      <tr class="HyhTu3ALpG">
+        <td class="Skaad3AUTM">data 3.1</td>
+      
+        <td class="HJA6d3A8pM">data 3.2</td>
+      
+        <td class="HJygTuh0U6M">data 3.3</td>
+      </tr>
+    </table>
+  </div>
+  <i class="SkslK0L6G material-icons">insert_emoticon</i>
+  <img class="B13eF0UTf" src="/static/vuegg-fam.svg"/>
+  <mdc-linear-progress class="rygwGFCU6f" indeterminate></mdc-linear-progress>
+  <mdc-linear-progress class="rJZvGtA86G" indeterminate></mdc-linear-progress>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#SyeP-ORITf {
+  --mdc-theme-primary: #673ab7;
+  --mdc-theme-secondary: #f44336;
+  --mdc-theme-background: #ffffff;
+  position: relative;
+  margin: auto;
+  background-color: #ffffff;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
+.rJBpd3CUpf {
+  padding: 0 8px;
+}
+
+.rk86d30Lpf {
+  padding: 0 8px;
+}
+
+.rJvTOhAUpz {
+  padding: 0 8px;
+}
+
+.H14pOhCLTG {
+  background-color: rgba(0, 0, 0, 0.12);
+}
+
+.Skaad3AUTM {
+  padding: 0 8px;
+}
+
+.HJA6d3A8pM {
+  padding: 0 8px;
+}
+
+.HJygTuh0U6M {
+  padding: 0 8px;
+}
+
+.HyhTu3ALpG {
+  background-color: rgba(0, 0, 0, 0.12);
+}
+
+.BkTOhRIaM {
+  background-color: #ffffff;
+  border-collapse: collapse;
+  border: 1px solid #dddddd;
+  text-align: left;
+  position: absolute;
+  width: 549px;
+  height: 300px;
+  top: 28px;
+  left: 335.5px;
+}
+
+.BJ3f_RUTG {
+  overflow: hidden;
+  border-color: rgba(0, 0, 0, 0.15);
+  border-style: solid;
+  border-width: 1px;
+  position: absolute;
+  width: 973px;
+  height: 466px;
+  top: 0px;
+  left: 0px;
+}
+
+.SkslK0L6G {
+  font-family: 'Material Icons';
+  font-size: 32;
+  text-align: center;
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  top: 0px;
+  left: 0px;
+}
+
+</style>
